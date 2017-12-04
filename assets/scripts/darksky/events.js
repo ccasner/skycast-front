315f55d5606310237047e418afc487a0adb91b90
ccasner <charlottecasner@gmail.com>
2017-12-04 15:42:27 -0500

    Adds function for darksky form
    - added event handler to index.js for darksky submit button
    - Need to write function for ajax call that gets info from form as data
     to send to API call

---
+++ assets/scripts/darksky/events.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// const getFormFields = require('../../lib/get-form-fields')
+
+const onGetWeather = function (event) {
+  event.preventDefault()
+  console.log('button is working')
+}
+
+module.exports = {
+  onGetWeather
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,12 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 
+const darkEvents = require('./darksky/events')
+
 $(() => {
   setAPIOrigin(location, config)
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
+$(() => {
+  $('#darksky-search').on('submit', darkEvents.onGetWeather)
+})
