de11734a6923a75eba04c56ea5f3f6ebe206901e
ccasner <charlottecasner@gmail.com>
2017-12-06 14:15:19 -0500

    Updates variables for form data and function
    - changed variable for the location form  to 'city' from 'data'
    - changed function name of api call to 'showCity' from 'show'

--- assets/scripts/google/events.js
+++ assets/scripts/google/events.js
@@ -3,12 +3,15 @@
 const getFormFields = require('../../../lib/get-form-fields')
 
 const googleApi = require('./api')
+const googleUi = require('./ui')
 
 const onSearchCity = function (event) {
   event.preventDefault()
-  const data = getFormFields(event.target)
-  console.log(data)
-  googleApi.show(data)
+  const city = getFormFields(event.target)
+  console.log(city)
+  googleApi.showCity(city)
+    .then(googleUi.onSuccess)
+    .catch(googleUi.onFailure)
 }
 
 module.exports = {
