b5a773435917cf15405fa5b174c42ff38cadea34
ccasner <charlottecasner@gmail.com>
2017-12-08 02:40:09 -0500

    Gets current time to be sent to api to log
    - uses JS Math and Date objects to convert the current time into unix
    time
    - will send data to api if date is not specified by user

--- assets/scripts/google/events.js
+++ assets/scripts/google/events.js
@@ -7,8 +7,11 @@ const googleUi = require('./ui')
 const onSearchCity = function (event) {
   event.preventDefault()
   const city = getFormFields(event.target)
-  console.log(city)
-  googleApi.showCity(city)
+  const address = city.address
+  const unixTime = Math.round((new Date()).getTime() / 1000)
+  const time = unixTime.toString()
+  console.log(time)
+  googleApi.showCity(address, time)
     .then(googleUi.onSuccess)
     .catch(googleUi.onFailure)
 }
