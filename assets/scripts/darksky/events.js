ecd511d6fce23ca226101fbcec1516c6748f4142
ccasner <charlottecasner@gmail.com>
2017-12-08 02:34:25 -0500

    Adds time to ajax call
    - set up function to accept a time to send to api
    - needs to be completed, currently hardcoded data set for testing

--- assets/scripts/darksky/events.js
+++ assets/scripts/darksky/events.js
@@ -6,13 +6,12 @@ const darkUi = require('./ui')
 
 const onGetWeather = function (event) {
   event.preventDefault()
-
   const data = getFormFields(event.target)
-  const long = data.long
-  const lat = data.lat
-  console.log(event.target)
-  console.log(data)
-  darkApi.showWeather(lat, long)
+  console.log(data.time)
+  const unixTime = new Date(data.time).getTime() / 1000
+  console.log(unixTime)
+  const id = 45
+  darkApi.showDay(id, unixTime)
     .then(darkUi.onSuccess)
     .catch(darkUi.onFailure)
 }
